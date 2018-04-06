67415fe2ec8dab4d3eb6de02df725559b962007f
Sanjana2000 <sanjana100@gmail.com>
2018-04-05 23:00:38 -0700

    revised

--- app.js
+++ app.js
@@ -4,38 +4,47 @@ const toDoList = document.getElementById('toDoList');
 
 function onReady() {
   let toDos = [];
-
+ let id= 0;
   function createNewToDo() {
     if (!newToDoText.value) {
       return;
     }
     toDos.push({
       title: newToDoText.value,
-      complete: false
-    });
+      complete: false,
+      id:id
 
+    });
+     id++;
     newToDoText.value = '';
     renderTheUI();
 
 
   }
+ function deleteToDo (id){
 
+   toDos = toDos.filter(toDo => toDo.id !==id);
+   renderTheUI();
+ }
   function renderTheUI() {
     const toDoList = document.getElementById('toDoList');
 
     toDoList.textContent = '';
 
     toDos.forEach(function(toDo) {
-      const newToDo = document.createElement('li');
+      const newLi = document.createElement('li');
 
       const checkbox = document.createElement('input');
       checkbox.type = "checkbox";
 
-      const title = document.createElement('span');
-      title.textContent = toDo.title;
+      const deleteButton = document.createElement('button');
 
+    deleteButton.addEventListener('click', () => deleteToDo(toDo.id));
+    newLi.textContent = toDo.title;
+     deleteButton.textContent = "Delete";
       toDoList.appendChild(newLi);
       newLi.appendChild(checkbox);
+      newLi.appendChild(deleteButton);
     });
 
   }
