85cbe09c56c162ee1932a0efbf6d777dedc488eb
Sanjana2000 <sanjana100@gmail.com>
2018-04-05 23:18:23 -0700

    fixed version

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const toDoList = document.getElementById('toDoList');
 
 function onReady() {
   let toDos = [];
- let id= 0;
+  let id = 0;
+
   function createNewToDo() {
     if (!newToDoText.value) {
       return;
@@ -12,20 +13,22 @@ function onReady() {
     toDos.push({
       title: newToDoText.value,
       complete: false,
-      id:id
+      id: id
 
     });
-     id++;
+    id++;
     newToDoText.value = '';
     renderTheUI();
 
 
   }
- function deleteToDo (id){
 
-   toDos = toDos.filter(toDo => toDo.id !==id);
-   renderTheUI();
- }
+  function deleteToDo(id) {
+
+    toDos = toDos.filter(toDo => toDo.id !== id);
+    renderTheUI();
+  }
+
   function renderTheUI() {
     const toDoList = document.getElementById('toDoList');
 
@@ -39,24 +42,19 @@ function onReady() {
 
       const deleteButton = document.createElement('button');
 
-    deleteButton.addEventListener('click', () => deleteToDo(toDo.id));
-    newLi.textContent = toDo.title;
-     deleteButton.textContent = "Delete";
+      deleteButton.addEventListener('click', () => deleteToDo(toDo.id));
+      newLi.textContent = toDo.title;
+      deleteButton.textContent = "Delete";
       toDoList.appendChild(newLi);
       newLi.appendChild(checkbox);
       newLi.appendChild(deleteButton);
     });
-
-  }
+}
 
   addToDoForm.addEventListener('submit', event => {
     event.preventDefault();
     createNewToDo();
-
-
   });
-
-
 }
 
 window.onload = function() {
